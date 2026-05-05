fd633670b935b233aecb58013e0d50f7d51e4c5c
fatih <fatih.maki10@gmail.com>
2026-05-05 13:34:55 +0700

    init project rumah makan + migration

---
+++ database/migrations/2026_02_10_074151_create_catering_items_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('catering_items', function (Blueprint $table) {
+            $table->id();
+    
+            $table->foreignId('catering_id')
+                  ->constrained()
+                  ->cascadeOnDelete();
+    
+            $table->foreignId('menu_id')
+                  ->constrained()
+                  ->restrictOnDelete();
+    
+            $table->integer('qty');
+            $table->integer('price');
+            $table->integer('subtotal');
+    
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('catering_items');
+    }
+};
