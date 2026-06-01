8d2bbf4ffe90f716a82fc3ddec45181eb85d7788
gamer <ilhamgoesasi@gmail.com>
2026-06-01 22:24:51 +0700

    feat: create order confirmation page with payment gateway integration

--- rm-kembar/resources/js/Pages/Orders/Confirm.jsx
+++ rm-kembar/resources/js/Pages/Orders/Confirm.jsx
@@ -52,6 +52,7 @@ export default function Confirm({ order }) {
                                 >
                                     Bayar Sekarang
                                 </button>
+                            )}
                         </div>
                     )}
                 </div>
